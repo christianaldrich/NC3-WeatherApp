52c5de06642a9264d7f97af80b37c180bf9efabf
christianaldrich <pang.aldrich@gmail.com>
2024-07-10 10:38:22 +0700

    CoreLocation Component

---
+++ CoreLocationComponent/CoreLocationComponent/CoreLocationComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLocationComponentApp.swift
+//  CoreLocationComponent
+//
+//  Created by Christian Aldrich Darrien on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreLocationComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoreLocationComponent/CoreLocationComponent/LocationManager.swift
@@ -0,0 +1,50 @@
+//
+//  LocationManager.swift
+//  CoreLocationComponent
+//
+//  Created by Christian Aldrich Darrien on 10/07/24.
+//
+
+import Foundation
+import CoreLocation
+
+
+class TempModel : NSObject, ObservableObject, CLLocationManagerDelegate{
+    
+    private var locationManager : CLLocationManager?
+    
+    @Published var longitude:Double = 0.0
+    @Published var latitude:Double = 0.0
+    
+    override init() {
+        super.init()
+        checkIfLocationServiceIsEnabled()
+    }
+    
+    func checkIfLocationServiceIsEnabled(){
+        if CLLocationManager.locationServicesEnabled() {
+                    locationManager = CLLocationManager()
+                    locationManager?.delegate = self
+                    locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+                    locationManager?.requestWhenInUseAuthorization()
+                    locationManager?.startUpdatingLocation()
+                } else {
+                    print("Show an alert letting them know this is off and to go turn it on.")
+                }
+        }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last{
+            latitude = location.coordinate.latitude
+            longitude = location.coordinate.longitude
+            print("Lat : \(latitude)")
+            print("Long : \(longitude)")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+            print("Failed to find user's location: \(error.localizedDescription)")
+        }
+    
+    
+}
