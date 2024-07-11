4ba94b3b7221c2659041ce819b2b5f8a5544e23a
Cindaa10 <cindaarta16@gmail.com>
2024-07-11 20:24:04 +0700

    Add optimal time in content view

---
+++ CoreLocationComponent/CoreLocationComponent/View/OptimalTime.swift
@@ -0,0 +1,18 @@
+//
+//  OptimalTime.swift
+//  CoreLocationComponent
+//
+//  Created by Lucinda Artahni on 11/07/24.
+//
+
+import SwiftUI
+
+struct OptimalTime: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OptimalTime()
+}
