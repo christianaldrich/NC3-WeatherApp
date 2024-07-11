feb93e60f58076e3b71af4844a61f546688ac377
NJuGau <nathanael.gauthama@binus.ac.id>
2024-07-11 16:55:39 +0700

    Done Getting safe weather and also splitting weather into today and tomorrow

---
+++ CoreLocationComponent/CoreLocationComponent/Model/Time.swift
@@ -0,0 +1,23 @@
+//
+//  Time.swift
+//  CoreLocationComponent
+//
+//  Created by Nathanael Juan Gauthama on 11/07/24.
+//
+
+import Foundation
+
+
+struct TimeRange: Identifiable, Hashable {
+    let id = UUID()
+    var startTime: Date
+    var endTime: Date
+    
+    static func == (lhs: TimeRange, rhs: TimeRange) -> Bool {
+        return lhs.id == rhs.id
+    }
+        
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
